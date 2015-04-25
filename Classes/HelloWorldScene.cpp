eeb1961ff86c013632c0e7fc2c30262233b70574
younger.shen <younger.x.shen@gmail.com>
2015-04-25 18:14:13 +0800

    config

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -54,7 +54,7 @@ bool HelloWorld::init()
     // add a label shows "Hello World"
     // create and initialize a label
     
-    auto label = Label::createWithTTF("fuck world", "fonts/Marker Felt.ttf", 24);
+    auto label = Label::createWithTTF("fuck worlsd", "fonts/Marker Felt.ttf", 24);
     
     // position the label on the center of the screen
     label->setPosition(Vec2(origin.x + visibleSize.width/2,
